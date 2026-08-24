9fe47d0de0681e738a0d939465a312156cf0f902
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-08-24 18:52:28 +0530

    useRouterError

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,9 +1,15 @@
+import { useRouteError } from 'react-router-dom'
 import './Error.css' 
 
 export default function Error(){
+
+    const error = useRouteError() 
+    console.log(error.status);
+    
     return(
         <>
-        <h1>An Error occurred please try again !</h1>
+        <h1>An Error occurred please try again !</h1> 
+        <h1>{error.status} you got a {error.message} </h1>
         </>
     )
 }
